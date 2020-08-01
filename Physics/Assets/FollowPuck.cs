f7bd600d78831c8425f9af98daf1e68b56815f99
Jan Reichelt <janreichelt@I-0022s-MBP.fritz.box>
2020-08-01 09:21:24 +0200

    added physics project

---
+++ Physics/Assets/FollowPuck.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class FollowPuck : MonoBehaviour {
+
+    public Transform puck;
+    public Vector3 offset;
+
+    void Update() {
+        transform.position = puck.position + offset;
+    }
+}
---
+++ Physics/Assets/dragflip.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class dragflip : MonoBehaviour {
+
+
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log("Hi, das ist mein erstes Hello World!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
