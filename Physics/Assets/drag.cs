3a1670e4a5c2d619a660433c0010b748f51d1a74
Jan Reichelt <janreichelt@I-0022s-MBP.fritz.box>
2020-08-03 08:14:28 +0200

    added first crude flip functionality

---
+++ Physics/Assets/drag.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drag : MonoBehaviour
+{
+    // Reference: https://www.youtube.com/watch?v=0yHBDZHLRbQ
+
+    private Vector3 mOffset;
+    private float mZCoord;
+
+    private void OnMouseDrag() {
+        transform.position = GetMouseWorldPos() + mOffset;
+    }
+
+    private void OnMouseDown() {
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+    }
+
+    private Vector3 GetMouseWorldPos() {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+}
--- Physics/Assets/dragflip.cs
+++ Physics/Assets/dragflip.cs
@@ -2,15 +2,30 @@
 
 public class dragflip : MonoBehaviour {
 
+    private float mZCoord;
+    private Vector3 mOffset;
+    private Vector3 draggedDistance;
+    private Rigidbody rb;
 
-    // Start is called before the first frame update
-    void Start() {
-        Debug.Log("Hi, das ist mein erstes Hello World!!");
+    private void OnMouseDown() {
+        rb      = this.GetComponent<Rigidbody>();
+        mZCoord = Camera.main.WorldToScreenPoint(transform.position).z;
+        mOffset = transform.position - GetMouseWorldPos();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+      private Vector3 GetMouseWorldPos() {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    private void OnMouseDrag() {
+        draggedDistance = transform.position - GetMouseWorldPos();
+    }
+
+    private void OnMouseUp() {
+        rb.AddForce(draggedDistance*500);
+        //transform.position += draggedDistance;
     }
 }
